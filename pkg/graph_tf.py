0a6bf476ba1ebbc347e0e57e2cd75deb850bf7a0
Junsu Kang <cucumberkjs@gmail.com>
2020-08-03 21:17:31 +0900

    ConstructSearchGraph done

--- pkg/graph_tf.py
+++ pkg/graph_tf.py
@@ -23,32 +23,20 @@ from pkg.binding_calculator import *
 
 class GraphModel(tf.keras.Model):
     def __init__(self, robot_info, gitem_list, binfo_list, urdf_content, N_sim, rate_update=0.9, alpha_lpf=0.9, error_margin=1e-4,
-                 alpha_jc=5, alpha_fc=200, alpha_jl=1, alpha_cl=1,alpha_cs=0, dQ_max = tf.constant([[1.0]*6])*1e-1, COL_AVOID_MULTIPLIER=1.0,
-                 LIM=np.pi, LIM_BOUND=1e-1, COL_BOUND=1e-2, learning_rate=5e-3, col_iteration = 20):
+                 dQ_max = tf.constant([[1.0]*6])*1e-1, COL_AVOID_MULTIPLIER=1.0):
         super(GraphModel, self).__init__()
-        self.alpha_jc = alpha_jc
-        self.alpha_fc = alpha_fc
-        self.alpha_jl = alpha_jl
-        self.alpha_cl = alpha_cl
-        self.alpha_cs = alpha_cs
         self.rate_update = rate_update
         self.alpha_lpf = alpha_lpf
         self.error_margin = error_margin
         self.N_sim = N_sim
-        self.LIM = LIM
-        self.LIM_BOUND = LIM_BOUND
-        self.COL_BOUND = COL_BOUND
         self.COL_AVOID_MULTIPLIER = COL_AVOID_MULTIPLIER
         self.dQ_max = tf.constant(dQ_max)
-        self.learning_rate = learning_rate
-        self.col_iteration = col_iteration
         self.robot_info = robot_info
         self.robot = RobotLayer(
             robot_info, dim=N_sim)
         self.joint_constraint = JointConstraintLoss(self.robot)
         self.frame_constraint = FrameConstraintLoss()
         self.robot_base = [robot_info.base_frame]*N_sim
-        self.set_custom_loss(lambda *args: tf.constant(0.0), 0)
         self.link_name_list = self.robot.link_name_list
         self.link_idx_dict = self.robot.link_idx_dict
         self.adjacency_list = self.robot.adjacency_list
@@ -85,7 +73,6 @@ class GraphModel(tf.keras.Model):
         
         self.col_cal = CollisionCalculator(self.object_name_list, self.object_dict, self.robot, self.N_sim)
         self.bind_cal = BindingCalculator(self.binding_name_list, self.binding_dict, self.robot, self.N_sim)
-        self.optimizer = tf.optimizers.SGD(learning_rate=learning_rate)
             
     def assign_frame_dict(self, gframeset_list):
         self.gframeset_list = gframeset_list
@@ -156,77 +143,6 @@ class GraphModel(tf.keras.Model):
         Tbb_all = K.stack(Tbb_all, axis=1) #(N_sim,Nobj,4,4)
         return T_all, Tbo_all, Tbb_all
     
-    @tf.function
-    def calc_joint_limit(self):
-        Q = self.get_Q()
-        return K.sum(1/((K.min(((self.LIM-Q), (self.LIM+Q)), axis=-1)/self.LIM_BOUND)**3))
-    
-    @tf.function
-    def calc_loss(self, T_all, Tbo_all, Qtar, binQ, Ttar, binT):
-        jl_loss = self.calc_joint_limit()
-        jc_loss = self.joint_constraint((Qtar, binQ))
-        fc_loss = self.frame_constraint((T_all[:,-1,:,:],Ttar, binT))
-        #cl_loss = self.calc_collision_loss(T_all, Tbo_all)
-        return self.alpha_jc*jc_loss + self.alpha_fc*fc_loss + \
-                self.alpha_jl*jl_loss + self.alpha_cs*self.calc_custom_loss(T_all, Tbo_all) \
-                #+ self.alpha_cl*cl_loss 
-                
-    
-    @tf.function
-    def forward(self, Qtar, binQ, Ttar, binT):
-        T_all, Tbo_all= self(None)
-        loss = self.calc_loss(T_all, Tbo_all, Qtar, binQ, Ttar, binT)
-        return loss
-    
-    @tf.function
-    def update_once(self, Qtar, binQ, Ttar, binT, max_gradient):
-        with tf.GradientTape() as g:
-            # Forward pass.
-            loss = self.forward(Qtar, binQ, Ttar, binT)
-
-        # Variables to update, i.e. trainable variables.
-        trainable_variables = self.trainable_variables
-
-        # Compute gradients.
-        gradients = g.gradient(loss, trainable_variables)
-        gradients = tf.unstack(clip_gradient(gradients, max_gradient))
-    
-        # Update W and b following gradients.
-        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
-        return loss
-    
-    def set_custom_loss(self, custom_loss, alpha_cs, *args, **kwargs):
-        self.custom_loss = custom_loss
-        self.alpha_cs = alpha_cs
-        self.cl_args = args
-        self.cl_kwargs = kwargs
-        
-    def calc_custom_loss(self, T_all, Tbo_all):
-        return self.custom_loss(self, T_all, Tbo_all, *self.cl_args, **self.cl_kwargs)
-
-    @tf.function
-    def test_collision(self, T_all, Tbo_all):
-        Pbo_all = tf.gather(tf.gather(Tbo_all, [0,1,2], axis=-2), 3, axis=-1)
-        col_combs_masked_batch = self.get_collision_combination()
-        batch_flat = tf.repeat(np.reshape(np.arange(self.N_sim), (self.N_sim, 1)), (self.N_col,)*self.N_sim,axis=0)
-        col_combs_masked_flat = tf.concat([batch_flat, col_combs_masked_batch], axis=-1)
-        vtx_tf = tf.gather(
-            K.sum(tf.expand_dims(Tbo_all, axis=-3)*tf.expand_dims(self.object_vertice_mat, axis=-2), axis=-1), 
-            [0,1,2], axis=-1)
-        vtx_tf_1 = tf.reshape(tf.gather_nd(vtx_tf, tf.gather(col_combs_masked_flat, [0,1], axis=1)), (self.N_sim, self.N_col, -1,3))
-        vtx_tf_2 = tf.reshape(tf.gather_nd(vtx_tf, tf.gather(col_combs_masked_flat, [0,2], axis=1)), (self.N_sim, self.N_col, -1,3))
-        Pbo_all_1 = tf.reshape(tf.gather_nd(Pbo_all, tf.gather(col_combs_masked_flat, [0,1], axis=1)), (self.N_sim, self.N_col,3))
-        Pbo_all_2 = tf.reshape(tf.gather_nd(Pbo_all, tf.gather(col_combs_masked_flat, [0,2], axis=1)), (self.N_sim, self.N_col,3))
-        v_batch = tf.expand_dims(Pbo_all_1-Pbo_all_2, axis=-2)
-        v_batch = tf.stop_gradient(tf.linalg.cross(v_batch, self.x_batch)+tf.linalg.cross(v_batch, self.y_batch))
-        dist, flag = test_collision_batch(vtx_tf_1, vtx_tf_2, 
-                             v_batch, self.flag_default, self.dist_default, IterationAllowed=self.col_iteration)
-        return dist, flag, col_combs_masked_flat 
-    
-    @tf.function
-    def calc_collision_loss(self, T_all, Tbo_all):
-        dist, flag, _  = self.test_collision(T_all, Tbo_all)
-        return K.sum(1/((dist/self.COL_BOUND)**3))
     
     def jacobian(self, T_all, Tbo_all, Tbb_all):
         P_jnt, axis_jnt = self.robot.prepare_joint_axis(T_all)
