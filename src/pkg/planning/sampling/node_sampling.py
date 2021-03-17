d663b70283d2279d26e2086081072f9e3e79c695
JunsuKang <cucumberkjs@gmail.com>
2021-03-17 17:14:21 +0900

    file-arranged, commented

--- src/pkg/planning/sampling/node_sampling.py
+++ src/pkg/planning/sampling/node_sampling.py
@@ -63,3 +63,91 @@ class UniformNodeSampler:
         with self.prob_lock:
             self.log2_prob_dict[node] = self.log2_prob_dict[node] - self.log2_decay
         return node
+
+
+##
+# @class GrowingSampler
+# @biref Grow sampler keys as new items are added, and sample uniformly
+class GrowingSampler:
+    def __init__(self, log2_decay=1):
+        self.log2_decay = log2_decay
+
+    def init(self, tplan, multiprocess_manager):
+        self.node_dict = tplan.node_dict
+        self.multiprocess_manager = multiprocess_manager
+        if multiprocess_manager is not None:
+            self.log2_prob_dict = multiprocess_manager.dict()
+            self.prob_lock = multiprocess_manager.Lock()
+        else:
+            self.log2_prob_dict = dict()
+            self.prob_lock = DummyBlock()
+
+    def __call__(self, nodes):
+        nodes = list(nodes)
+        for node in nodes:
+            if node not in self.log2_prob_dict:
+                self.log2_prob_dict[node] = 1.0
+        probs = [np.exp2(self.log2_prob_dict[node]) for node in nodes]
+        sumprobs = np.sum(probs)
+        if sumprobs == 0:
+            print("probability saturated")
+            #             probs = np.ones_like(probs)/len(probs)
+            with self.prob_lock:
+                for k in self.node_dict.keys():
+                    self.log2_prob_dict[k] = 1.0
+        else:
+            probs = np.divide(probs, sumprobs)
+        i_node = np.random.choice(len(nodes), p=probs)
+        node = nodes[i_node]
+        with self.prob_lock:
+            self.log2_prob_dict[node] = self.log2_prob_dict[node] - self.log2_decay
+        return node
+
+
+##
+# @class PenaltyNodeSampler
+# @biref Give penalty for used samples
+class PenaltyNodeSampler:
+    def __init__(self, log2_decay=1, log2_count_decay=1):
+        self.log2_decay = log2_decay
+        self.log2_count_decay = log2_count_decay
+
+    def init(self, tplan, multiprocess_manager):
+        self.node_dict = tplan.node_dict
+        self.multiprocess_manager = multiprocess_manager
+        if multiprocess_manager is not None:
+            self.log2_prob_dict = multiprocess_manager.dict()
+            self.log2_count_dict = multiprocess_manager.dict()
+            self.prob_lock = multiprocess_manager.Lock()
+        else:
+            self.log2_prob_dict = dict()
+            self.log2_count_dict = dict()
+            self.prob_lock = DummyBlock()
+
+        for k in self.node_dict.keys():
+            self.log2_prob_dict[k] = 1.0
+            self.log2_count_dict[k] = 0.0
+
+    def __call__(self, nodes):
+        nodes = list(nodes)
+        probs = [np.exp2(self.log2_prob_dict[node] + self.log2_count_dict[node]) for node in nodes]
+        sumprobs = np.sum(probs)
+        if sumprobs == 0:
+            print("probability saturated")
+            #             probs = np.ones_like(probs)/len(probs)
+            with self.prob_lock:
+                for k in self.node_dict.keys():
+                    self.log2_prob_dict[k] = 1.0
+                    self.log2_count_dict[k] = 0.0
+        else:
+            probs = np.divide(probs, sumprobs)
+        i_node = np.random.choice(len(nodes), p=probs)
+        node = nodes[i_node]
+        with self.prob_lock:
+            self.log2_prob_dict[node] = self.log2_prob_dict[node] - self.log2_decay
+        return node
+
+    def update(self, snode_src, snode_new, connection_result):
+        if connection_result:
+            self.log2_count_dict[snode_new.state.node] = self.log2_count_dict[
+                                                             snode_new.state.node] - self.log2_count_decay
