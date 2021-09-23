2c7f149c6850ee020c03971ff24902d24510f0c0
JunsuKang <zzanggjs@postech.ac.kr>
2021-09-23 11:25:10 +0900

    check feasibility interface function integrated

--- src/pkg/planning/filtering/filter_interface.py
+++ src/pkg/planning/filtering/filter_interface.py
@@ -16,11 +16,16 @@ class MotionFilterInterface:
     # @param handle rnb-planning.src.pkg.planning.constraint.constraint_common.ActionPoint
     # @param redundancy_values calculated redundancy values in dictionary format {(object name, point name): (xyz, rpy)}
     # @param Q_dict joint configuration in dictionary format {joint name: radian value}
-    # @param interpolate    interpolate path and check intermediate poses
     # @return True if feasible, else False
     @abstractmethod
-    def check(self, actor, obj, handle, redundancy_values, Q_dict, interpolate=False):
-        pass
+    def check(self, actor, obj, handle, redundancy_values, Q_dict, **kwargs):
+        point_add_handle, rpy_add_handle = redundancy_values[(obj.oname, handle.name)]
+        point_add_actor, rpy_add_actor = redundancy_values[(obj.oname, actor.name)]
+        T_handle_lh = np.matmul(handle.Toff_lh, SE3(Rot_rpy(rpy_add_handle), point_add_handle))
+        T_actor_lh = np.matmul(actor.Toff_lh, SE3(Rot_rpy(rpy_add_actor), point_add_actor))
+        T_loal = np.matmul(T_handle_lh, SE3_inv(T_actor_lh))
+
+        return self.check_T_loal(actor, obj, T_loal, Q_dict, **kwargs)
 
     ##
     # @brief define lock if it needs lock in multiprocess calls
--- src/pkg/planning/filtering/grasp_filter.py
+++ src/pkg/planning/filtering/grasp_filter.py
@@ -50,27 +50,6 @@ class GraspChecker(MotionFilterInterface):
                 self.link_robot_dict[lname] = rname
         # self.verts_holder_dict = {}
 
-    ##
-    # @brief check end-effector collision in grasping
-    # @param actor  rnb-planning.src.pkg.planning.constraint.constraint_actor.Actor
-    # @param obj    rnb-planning.src.pkg.planning.constraint.constraint_subject.Subject
-    # @param handle rnb-planning.src.pkg.planning.constraint.constraint_common.ActionPoint
-    # @param redundancy_values calculated redundancy values in dictionary format {(object name, point name): (xyz, rpy)}
-    # @param Q_dict joint configuration in dictionary format {joint name: radian value}
-    # @param interpolate    interpolate path and check intermediate poses
-    # @param ignore         GeometryItems to ignore
-    def check(self, actor, obj, handle, redundancy_values, Q_dict, interpolate=False, obj_only=False, ignore=[],
-              **kwargs):
-        # gtimer = GlobalTimer.instance()
-        # gtimer.tic("get_grasping_vert_infos")
-        point_add_handle, rpy_add_handle = redundancy_values[(obj.oname, handle.name)]
-        point_add_actor, rpy_add_actor = redundancy_values[(obj.oname, actor.name)]
-        T_handle_lh = np.matmul(handle.Toff_lh, SE3(Rot_rpy(rpy_add_handle), point_add_handle))
-        T_actor_lh = np.matmul(actor.Toff_lh, SE3(Rot_rpy(rpy_add_actor), point_add_actor))
-        T_loal = np.matmul(T_handle_lh, SE3_inv(T_actor_lh))
-
-        return self.check_T_loal(actor, obj, T_loal, Q_dict, interpolate=interpolate, obj_only=obj_only, ignore=ignore,
-                          **kwargs)
 
     ##
     # @brief check end-effector collision in grasping
