ebe98278158b4e9015c511ae350a1547f07e1981
JonghyeokKim <sentojh@postech.ac.kr>
2021-11-21 23:23:08 +0900

    add multiICP detector, Detection.md, usage scripts

---
+++ src/pkg/detector/multiICP/shared_detector.py
@@ -0,0 +1,85 @@
+import SharedArray as sa
+import numpy as np
+import cv2
+import time
+import os
+import sys
+import subprocess
+RNB_PLANNING_DIR = os.environ["RNB_PLANNING_DIR"]
+sys.path.append(os.path.join(os.path.join(RNB_PLANNING_DIR, 'src')))
+if sys.version.startswith("3"):
+    from pkg.utils.utils_python3 import *
+elif sys.version.startswith("2"):
+    from pkg.utils.utils import *
+
+from pkg.utils.shared_function import shared_fun, CallType, ArgSpec, ResSpec, set_serving, is_serving, serve_forever, SHARED_FUNC_ALL
+
+IMG_URI = "shm://color_img"
+MASK_URI = "shm://mask_img"
+REQ_URI = "shm://request"
+RESP_URI = "shm://response"
+
+IMG_DIM = (720, 1280, 3)
+# IMG_DIM = (1080, 1920, 3)
+
+FILE_PATH = os.path.join(RNB_PLANNING_DIR, 'src/pkg/detector/multiICP/shared_detector.py')
+
+class SharedDetector:
+    def __init__(self):        self.initizlied = False
+
+    @shared_fun(CallType.SYNC, "SharedDetector")
+    def init(self):
+        if not self.initizlied:
+            self.initizlied = True
+            # Load config, checkpoint file of cascade mask rcnn swin based
+            config_file = os.path.join(RNB_PLANNING_DIR,
+                                       'src/configs/detection_configs/swin/cascade_mask_rcnn_swin_base_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco.py')
+            checkpoint_file = os.path.join(RNB_PLANNING_DIR,
+                                           'src/configs/cascade_mask_rcnn_swin_base_patch4_window7.pth')
+
+            # config_file = '/home/jhkim/Swin-Transformer-Object-Detection/configs/swin/cascade_mask_rcnn_swin_base_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco.py'
+            # checkpoint_file = '/home/jhkim/Swin-Transformer-Object-Detection/cascade_mask_rcnn_swin_base_patch4_window7.pth'
+            device = 'cuda:0'
+            # device = 'cpu'
+            try:
+                self.model = init_detector(config_file, checkpoint_file, device=device)
+            except Exception as e:
+                TextColors.RED.println("[ERROR] Could not initialize detector")
+                raise(e)
+
+    @shared_fun(CallType.SYNC, "SharedDetector",
+                ArgSpec("color_img", IMG_DIM, np.uint8),
+                ResSpec(0, (80, IMG_DIM[0],IMG_DIM[1]), float))
+    def inference(self, color_img):
+        if self.initizlied:
+            result = inference_detector(self.model, color_img)
+            boxes, masks = result[0], result[1]
+
+            detect_false = np.empty((IMG_DIM[0],IMG_DIM[1]), dtype=bool)
+            detect_false[:, :] = False
+
+            return_img = np.zeros((80, IMG_DIM[0],IMG_DIM[1]))
+
+            for idx in range(80):
+                if len(masks[idx]) != 0:
+                    mask_res = masks[idx][0]
+                    return_img[idx][:,:] = mask_res
+                else:
+                    return_img[idx][:,:] = detect_false
+
+            return return_img
+        else:
+            raise(RuntimeError("[Error] Not initilized"))
+
+
+if __name__ == "__main__":
+    try:
+        from mmdet.apis import init_detector, inference_detector
+    except Exception as e:
+        print(TextColors.RED.println("[ERROR] Could not import mmdet"))
+        print(e)
+    sdet = SharedDetector()
+    set_serving(True)
+    serve_forever("SharedDetector", [sdet.inference, sdet.init], verbose=True)
+else:
+    output = subprocess.Popen(['python3', FILE_PATH], cwd=os.path.dirname(FILE_PATH))
